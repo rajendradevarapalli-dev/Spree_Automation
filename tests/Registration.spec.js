00afc521f280a2d5aedc4fed552b2cccde756921
Rajendra Devarapalli <rajendradevaraplli@gmail.com>
2026-05-20 14:49:23 +0530

    changed email registration

--- tests/Registration.spec.js
+++ tests/Registration.spec.js
@@ -2,13 +2,13 @@ import{test,expect} from '@playwright/test'
 import { registrationPage } from '../Pages/Registration.po'
 import { testData } from '../testdata/utils'
 
-//let random = (Math.random() + 1).toString(36).substring(7);
+let random = (Math.random() + 1).toString(36).substring(7);
 
 test('Verify User Registration with valid data',async ({page})=>{
 
      const registration = new registrationPage(page)
      await registration.launchURL()
-     await registration.signinWithDetials('rajendra','d','spree1224@example.com','123456','123456')
+     await registration.signinWithDetials('rajendra','d',`spree${random}@example.com`,'123456','123456')
      await registration.loginSuccess()
 
 
